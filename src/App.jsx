cea05cb71e85a1eb02731d199d7a46008940301d
Vladimir Vovk <57999167+VladimirVovk97@users.noreply.github.com>
2022-09-19 11:26:17 +0300

    App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,8 @@ import Profile from './Components/Profile/Profile';
 import user from './Components/Profile/user.json';
 import Statistics from './Components/Statistics/Statistics';
 import data from './Components/Statistics/data.json';
-// import FriendList from './Components/FriendList/Friendlist.styled';
-// import friends from './Components/FriendList/friends.json';
+import FriendList from './Components/FriendList/FriendList';
+import friends from './Components/FriendList/friends.json';
 // import Transactions from './Components/Transactions/Transactions';
 // import transactions from './Components/Transactions/transactions.json';
 
@@ -19,9 +19,9 @@ export default function App() {
         views={user.stats.views}
         likes={user.stats.likes}
       />
-      <Statistics title="Upload stats" stats={data} />;
+      <Statistics title="Upload stats" stats={data} />
+      <FriendList friendList={friends} />
       {/* <Statistics stats={data} />; */}
-      {/* <FriendList items={friends} /> */}
     </>
   );
 }
