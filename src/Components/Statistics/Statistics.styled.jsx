9879d80b84db98098dc6bb1e0a853a647ed36fb4
Vladimir Vovk <57999167+VladimirVovk97@users.noreply.github.com>
2022-09-19 11:21:31 +0300

    edit Statistics

--- src/Components/Statistics/Statistics.styled.jsx
+++ src/Components/Statistics/Statistics.styled.jsx
@@ -1,14 +1,16 @@
 import styled from '@emotion/styled';
 
 export const Section = styled.section`
-  border: 0.1px solid #afb1b8;
-  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 40px;
   text-align: center;
-  margin-top: 20px;
   width: 405px;
-  border-radius: 4px;
-  box-shadow: 0px 1px 3px rgba(0, 0, 0, 0.12), 0px 1px 1px rgba(0, 0, 0, 0.14),
-    0px 2px 1px rgba(0, 0, 0, 0.2);
+  background: #fff;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 10px;
 `;
 
 export const Title = styled.h2`
@@ -28,6 +30,9 @@ export const Item = styled.li`
   text-align: center;
   padding: 12px 20px;
   color: #ffffff;
+  background: #fff;
+  filter: drop-shadow(0px 4px 4px rgba(0, 0, 0, 0.25));
+  border-radius: 5px;
 `;
 export const Label = styled.span`
   display: block;
